1bb2a48d8c9e3854b702acaf4021479911483a37
Alan Busby <abusby@tako.japan.gracenote.com>
2014-09-08 12:08:11 +0900

    Switched to melpa-stable, because I'm not insane

--- my-lisp/setup-package.el
+++ my-lisp/setup-package.el
@@ -13,7 +13,7 @@
 
 ;; MELPA
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/"))
+             '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
 
 (package-initialize)
 
