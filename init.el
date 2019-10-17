710df05848afe14f760f2a43cabcdb996c2780d3
Alan Busby <thebusby@gmail.com>
2019-10-17 02:57:15 +0000

    Updated how MELPA is handled due to expired auth keys...

--- init.el
+++ init.el
@@ -11,7 +11,26 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
+(setq package-check-signature nil)
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl
+    (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
 (package-initialize)
+
 
 (setq my-lisp-dir
       (expand-file-name "my-lisp" user-emacs-directory))
@@ -136,7 +155,7 @@
 (require 'setup-paredit)
 (require 'setup-popwin)
 (require 'setup-mutt)
-(require 'setup-anything)
+; (require 'setup-anything)
 
 ;; Setup language/tool specific extensions
 (eval-after-load 'clojure-mode '(require 'setup-clojure-mode))
